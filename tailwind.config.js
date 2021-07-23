1d8db3020a9c478940fe20a92a4efa7222e29b76
gabrielsp20 <73500399+gabrielsp20@users.noreply.github.com>
2021-07-23 11:13:54 -0300

    Initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,29 @@
+module.exports = {
+  purge: [],
+  darkMode: false, // or 'media' or 'class'
+  theme: {
+    colors:{
+      'orange-1':'#E38826',
+      'cyan-1':'#006970',
+      'dark-cyan-1':'#004241',
+      'transparent-white-1':'hsla(0, 0%, 100%, 0.75)',
+      'Very-light-gray-1':'#F2F2F2',
+      'transp':'rgba(255, 0, 0, 0)',
+
+    },
+
+    fontFamily:{
+      'Big':['Big Shoulders Display, cursive'],
+      'Lexend':['Lexend Deca, sans-serif'],
+    },
+    extend: {
+      fontSize:{
+        'tx':'13px',
+      },
+    },
+  },
+  variants: {
+    extend: {},
+  },
+  plugins: [],
+}
